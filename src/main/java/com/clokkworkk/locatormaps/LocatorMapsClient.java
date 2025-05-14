8683913e6836c166c9c92ba2f32839db77675d23
CBSchiffer <CAS464@pitt.edu>
2025-05-14 16:44:16 -0400

    Initial commit

---
+++ src/main/java/com/clokkworkk/locatormaps/LocatorMapsClient.java
@@ -0,0 +1,10 @@
+package com.clokkworkk.locatormaps;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LocatorMapsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
