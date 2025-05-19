32f0a510237fc6a54742a84cf61c9b17bdf5e225
Caden Schiffer <CAS464@pitt.edu>
2025-05-18 23:20:47 -0700

    Some changes to map drawing in alternate dimensions (type shi)

--- src/main/java/com/clokkworkk/locatormaps/LocatorMapItem.java
+++ src/main/java/com/clokkworkk/locatormaps/LocatorMapItem.java
@@ -1,9 +1,7 @@
 package com.clokkworkk.locatormaps;
 
 import com.mojang.datafixers.util.Pair;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
-import net.minecraft.block.MapColor;
+import net.minecraft.block.*;
 import net.minecraft.component.DataComponentTypes;
 import net.minecraft.component.type.LoreComponent;
 import net.minecraft.entity.player.PlayerEntity;
@@ -27,8 +25,10 @@ import net.minecraft.text.Text;
 import net.minecraft.util.*;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
+import net.minecraft.world.Heightmap;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.Biome;
+import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.gen.structure.Structure;
 import org.jetbrains.annotations.Nullable;
 
@@ -100,10 +100,13 @@ public class LocatorMapItem extends NetworkSyncedItem {
                     boolean showCoords = itemStack.getOrDefault(LocatorMaps.MAP_SHOW_COORDS_COMPONENT, true);
                     BlockPos pos = strucPos.getFirst();
                     ItemStack map = FilledMapItem.createMap(world, pos.getX(), pos.getZ(), (byte)2, true, true);
-                    if(map.getOrDefault(LocatorMaps.DIMENSION_KEY_COMPONENT, "minecraft:overworld").equals("minecraft:overworld")) {
+                    LocatorMaps.LOGGER.info("Map created at: {}. Map is: {}", pos, map);
+                    if(itemStack.getOrDefault(LocatorMaps.DIMENSION_KEY_COMPONENT, "minecraft:overworld").equals("minecraft:overworld")) {
+                        LocatorMaps.LOGGER.info("Using default map fill");
                         FilledMapItem.fillExplorationMap(serverWorld, map);
                     } else {
-                        fillTerrain(serverWorld, map);
+                        LocatorMaps.LOGGER.info("Using custom map fill");
+                        fillTerrain(serverWorld, map, user.getY() < 0 ? 0 : (int) user.getY());
                     }
                     MapState.addDecorationsNbt(map, pos, "§a" + structureId.getPath(), MapDecorationTypes.RED_X);
 
@@ -199,83 +202,137 @@ public class LocatorMapItem extends NetworkSyncedItem {
         return structureName;
     }
 
-    private static void fillTerrain(ServerWorld world, ItemStack map) {
+//    private static void fillTerrain(ServerWorld world, ItemStack map) {
+//        MapState mapState = getMapState(map, world);
+//        if (mapState == null) return;
+//
+//        int scale = 1 << mapState.scale;
+//        int centerX = mapState.centerX;
+//        int centerZ = mapState.centerZ;
+//        int startX = centerX / scale - 64;
+//        int startZ = centerZ / scale - 64;
+//
+//        BlockPos.Mutable pos = new BlockPos.Mutable();
+//
+//        for (int dz = 0; dz < 128; dz++) {
+//            for (int dx = 0; dx < 128; dx++) {
+//                int worldX = (startX + dx) * scale;
+//                int worldZ = (startZ + dz) * scale;
+//
+//                // Get chunk directly
+//                Chunk chunk = world.getChunk(worldX >> 4, worldZ >> 4);
+//                BlockState state = chunk.getBlockState(pos.set(worldX, 32, worldZ));
+//
+//                boolean isEmpty = state.isAir() || state.getFluidState().isStill();
+//
+//                byte color = isEmpty
+//                        ? MapColor.LIGHT_GRAY.getRenderColorByte(MapColor.Brightness.LOW)
+//                        : MapColor.GREEN.getRenderColorByte(MapColor.Brightness.NORMAL);
+//
+//                mapState.setColor(dx, dz, color);
+//            }
+//        }
+//    }
+
+
+    private static void fillTerrain(ServerWorld world, ItemStack map, int ypos) {
         MapState mapState = getMapState(map, world);
         if (mapState != null) {
-            if (world.getRegistryKey() == mapState.dimension) {
-                int mapScale = 1 << mapState.scale;
-                int centerX = mapState.centerX;
-                int centerZ = mapState.centerZ;
-                boolean[] bls = new boolean[16384];
-                int startX = centerX / mapScale - 64;
-                int startZ = centerZ / mapScale - 64;
-                BlockPos.Mutable mutable = new BlockPos.Mutable();
+            int mapScale = 1 << mapState.scale;
+            int centerX = mapState.centerX;
+            int centerZ = mapState.centerZ;
+            boolean[] bls = new boolean[16384];
+            int startX = centerX / mapScale - 64;
+            int startZ = centerZ / mapScale - 64;
+            BlockPos.Mutable mutable = new BlockPos.Mutable();
+            boolean covered = false;
+            boolean checked = false;
+            BlockState state;
 
-                for (int dz = 0; dz < 128; dz++) {
-                    for (int dx = 0; dx < 128; dx++) {
-                        RegistryEntry<Biome> registryEntry = world.getBiome(mutable.set((startX + dx) * mapScale, 0, (startZ + dz) * mapScale));
-                        bls[dz * 128 + dx] = registryEntry.isIn(BiomeTags.WATER_ON_MAP_OUTLINES);
+            for (int dz = 0; dz < 128; dz++) {
+                for (int dx = 0; dx < 128; dx++) {
+                    int worldX = (startX + dx) * mapScale;
+                    int worldZ = (startZ + dz) * mapScale;
+                    Chunk chunk = world.getChunk(worldX >> 4, worldZ >> 4);
+                    if(!covered && !checked) {
+                        state = chunk.getBlockState(mutable.set(worldX, world.getTopY(Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, worldX, worldZ), worldZ));
+                        if(state.isOf(Blocks.BEDROCK)) {
+                            covered = true;
+                        }
+                        checked = true;
                     }
+                    if(covered) {
+                        state = chunk.getBlockState(mutable.set(worldX, ypos, worldZ));
+                    } else {
+                        state = chunk.getBlockState(mutable.set(worldX, ypos, worldZ));
+                    }
+
+                    bls[dz * 128 + dx] = state.isAir() || state.getFluidState().isStill();
+                    //LocatorMaps.LOGGER.info("BlockState at ({}, {}): {}", startX + dx, startZ + dz, state);
                 }
+            }
 
-                for (int z = 1; z < 127; z++) {
-                    for (int x = 1; x < 127; x++) {
-                        int neighbors = 0;
+            for (int z = 1; z < 127; z++) {
+                for (int x = 1; x < 127; x++) {
+                    int neighbors = 0;
 
-                        for (int dz = -1; dz < 2; dz++) {
-                            for (int dx = -1; dx < 2; dx++) {
-                                if ((dz != 0 || dx != 0) && isSeaOrSky(bls, z + dz, x + dx)) {
-                                    neighbors++;
-                                }
+                    for (int dz = -1; dz < 2; dz++) {
+                        for (int dx = -1; dx < 2; dx++) {
+                            if ((dz != 0 || dx != 0) && isSeaOrSky(bls, z + dz, x + dx)) {
+                                neighbors++;
                             }
                         }
+                    }
 
-                        MapColor.Brightness brightness = MapColor.Brightness.LOWEST;
-                        MapColor mapColor = MapColor.CLEAR;
-                        if (isSeaOrSky(bls, z, x)) {
-                            mapColor = MapColor.ORANGE;
-                            if (neighbors > 7 && x % 2 == 0) {
-                                switch ((z + (int)(MathHelper.sin(x + 0.0F) * 7.0F)) / 8 % 5) {
-                                    case 0:
-                                    case 4:
-                                        brightness = MapColor.Brightness.LOW;
-                                        break;
-                                    case 1:
-                                    case 3:
-                                        brightness = MapColor.Brightness.NORMAL;
-                                        break;
-                                    case 2:
-                                        brightness = MapColor.Brightness.HIGH;
-                                }
-                            } else if (neighbors > 7) {
-                                mapColor = MapColor.CLEAR;
-                            } else if (neighbors > 5) {
-                                brightness = MapColor.Brightness.NORMAL;
-                            } else if (neighbors > 3) {
-                                brightness = MapColor.Brightness.LOW;
-                            } else if (neighbors > 1) {
-                                brightness = MapColor.Brightness.LOW;
+                    MapColor.Brightness brightness = MapColor.Brightness.LOWEST;
+                    MapColor mapColor = MapColor.CLEAR;
+                    if (isSeaOrSky(bls, z, x)) {
+                        mapColor = MapColor.ORANGE;
+                        if (neighbors > 7 && x % 2 == 0) {
+                            switch ((z + (int)(MathHelper.sin(x + 0.0F) * 7.0F)) / 8 % 5) {
+                                case 0:
+                                case 4:
+                                    brightness = MapColor.Brightness.LOW;
+                                    break;
+                                case 1:
+                                case 3:
+                                    brightness = MapColor.Brightness.NORMAL;
+                                    break;
+                                case 2:
+                                    brightness = MapColor.Brightness.HIGH;
                             }
-                        } else if (neighbors > 0) {
-                            mapColor = MapColor.BROWN;
-                            if (neighbors > 3) {
-                                brightness = MapColor.Brightness.NORMAL;
-                            } else {
-                                brightness = MapColor.Brightness.LOWEST;
-                            }
+                        } else if (neighbors > 7) {
+                            mapColor = MapColor.CLEAR;
+                        } else if (neighbors > 5) {
+                            brightness = MapColor.Brightness.NORMAL;
+                        } else if (neighbors > 3) {
+                            brightness = MapColor.Brightness.LOW;
+                        } else if (neighbors > 1) {
+                            brightness = MapColor.Brightness.LOW;
                         }
+                    } else if (neighbors > 0) {
+                        mapColor = MapColor.BROWN;
+                        if (neighbors > 3) {
+                            brightness = MapColor.Brightness.NORMAL;
+                        } else {
+                            brightness = MapColor.Brightness.LOWEST;
+                        }
+                    }
 
-                        if (mapColor != MapColor.CLEAR) {
-                            mapState.setColor(z, x, mapColor.getRenderColorByte(brightness));
-                        }
+                    if (mapColor != MapColor.CLEAR) {
+                        mapState.setColor(z, x, mapColor.getRenderColorByte(brightness));
+                    } else {
+                        mapState.setColor(z, x, mapColor.getRenderColorByte(MapColor.Brightness.HIGH));
                     }
                 }
             }
+        } else {
+            LocatorMaps.LOGGER.error("MapState is null for map: {}", map);
         }
     }
 
-    private static boolean isSeaOrSky(boolean[] bls, int i, int i1) {
-
+    private static boolean isSeaOrSky(boolean[] bls, int z, int x) {
+        return bls[z * 128 + x];
     }
 
 
